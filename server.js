5e4f87391227ea062ab96ebd66e834dc6d1b40a6
adhitbodit <raka.adhitya9000@gmail.com>
2025-11-17 19:42:24 +0700

    server.js

--- server.js
+++ server.js
@@ -53,6 +53,7 @@ app.get("/commands", (req, res) => {
   res.json(data);
 });
 
-app.listen(3000, () => {
-  console.log("Server berjalan di port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server berjalan di port", PORT);
 });
